a91b6220082558c0ffb52e4c18ab320aed157867
Michael Sydney Balloni <balloni.michael@yahoo.com>
2022-06-04 16:56:29 -0700

    Started writing article, realized that basic send / recv is all that's needed Took a machete to the spec.txt Did away with the whole schema Will concentrate on clean c# for nailing the implementation and a useful article for good coding practices

--- docs/schema.sql
+++ docs/schema.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `msgfiles` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `msgfiles`;
+CREATE DATABASE  IF NOT EXISTS `msgs` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `msgs`;
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
--- Host: localhost    Database: msgfiles
+-- Host: localhost    Database: msgs
 -- ------------------------------------------------------
 -- Server version	8.0.28
 
@@ -18,27 +18,6 @@ USE `msgfiles`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `files`
---
-
-DROP TABLE IF EXISTS `files`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `files` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `namestringid` bigint NOT NULL,
-  `size` bigint NOT NULL,
-  `md5` varchar(32) COLLATE utf8mb4_bin NOT NULL,
-  `path` varchar(255) COLLATE utf8mb4_bin NOT NULL,
-  `created` timestamp NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `path_UNIQUE` (`path`),
-  KEY `fk_filename_string_ids_idx` (`namestringid`),
-  CONSTRAINT `fk_filename_string_ids` FOREIGN KEY (`namestringid`) REFERENCES `strings` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Table structure for table `msgs`
 --
 
@@ -46,81 +25,15 @@ DROP TABLE IF EXISTS `msgs`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `msgs` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `fromuserid` bigint NOT NULL,
-  `touserid` bigint NOT NULL,
-  `attachmentsnodeid` bigint NOT NULL,
-  `deletedfrom` bit(1) NOT NULL DEFAULT b'0',
-  `deletedto` bit(1) NOT NULL DEFAULT b'0',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `attachmentsnodeid_UNIQUE` (`attachmentsnodeid`),
-  KEY `fk_msg_from_idx` (`fromuserid`),
-  KEY `fk_msg_to_idx` (`touserid`),
-  CONSTRAINT `fk_msg_attachments` FOREIGN KEY (`attachmentsnodeid`) REFERENCES `nodes` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `fk_msg_from` FOREIGN KEY (`fromuserid`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `fk_msg_to` FOREIGN KEY (`touserid`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `nodes`
---
-
-DROP TABLE IF EXISTS `nodes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `nodes` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `parentid` bigint DEFAULT NULL,
-  `fileid` bigint DEFAULT NULL,
-  `userid` bigint NOT NULL,
-  `namestringid` bigint NOT NULL,
-  `created` timestamp NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_node_parentids_idx` (`parentid`),
-  KEY `fk_node_fileids_idx` (`fileid`),
-  KEY `fk_node_nameids_idx` (`namestringid`),
-  KEY `fk_node_users_idx` (`userid`),
-  CONSTRAINT `fk_node_fileids` FOREIGN KEY (`fileid`) REFERENCES `files` (`id`),
-  CONSTRAINT `fk_node_nameids` FOREIGN KEY (`namestringid`) REFERENCES `strings` (`id`),
-  CONSTRAINT `fk_node_parentids` FOREIGN KEY (`parentid`) REFERENCES `nodes` (`id`),
-  CONSTRAINT `fk_node_users` FOREIGN KEY (`userid`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `strings`
---
-
-DROP TABLE IF EXISTS `strings`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `strings` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `value` varchar(255) COLLATE utf8mb4_bin NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `value_UNIQUE` (`value`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_bin NOT NULL,
-  `privatenodeid` bigint NOT NULL,
-  `inboxnodeid` bigint NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name_UNIQUE` (`name`),
-  UNIQUE KEY `rootnodeid_UNIQUE` (`privatenodeid`),
-  UNIQUE KEY `inboxnodeid_UNIQUE` (`inboxnodeid`),
-  CONSTRAINT `fk_user_inboxnode` FOREIGN KEY (`inboxnodeid`) REFERENCES `nodes` (`id`),
-  CONSTRAINT `fk_user_privatenode` FOREIGN KEY (`privatenodeid`) REFERENCES `nodes` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+  `ticket` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `from` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `to` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `msg` text COLLATE utf8mb4_bin NOT NULL,
+  `filesize` bigint NOT NULL,
+  `filename` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `filepath` varchar(1024) COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`ticket`),
+  UNIQUE KEY `ticket_UNIQUE` (`ticket`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -133,4 +46,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-04 12:55:47
+-- Dump completed on 2022-06-04 16:36:37
