20fa8000a07a0bc06ca2739352f08dfbbd17309e
Michael Sydney Balloni <balloni.michael@yahoo.com>
2022-06-06 21:43:37 -0700

    Created client project, Send Files form Putting this to bed for now

---
+++ client/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.MessageTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.ToTextBox = new System.Windows.Forms.TextBox();
+            this.FromTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 453);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(399, 64);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Send Files";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(188, 408);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(223, 39);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Pick Directory..";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 408);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 39);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Pick Files...";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // MessageTextBox
+            // 
+            this.MessageTextBox.Location = new System.Drawing.Point(12, 144);
+            this.MessageTextBox.Multiline = true;
+            this.MessageTextBox.Name = "MessageTextBox";
+            this.MessageTextBox.Size = new System.Drawing.Size(399, 258);
+            this.MessageTextBox.TabIndex = 11;
+            this.MessageTextBox.UseSystemPasswordChar = true;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(12, 100);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(399, 38);
+            this.PasswordTextBox.TabIndex = 10;
+            this.PasswordTextBox.UseSystemPasswordChar = true;
+            // 
+            // ToTextBox
+            // 
+            this.ToTextBox.Location = new System.Drawing.Point(12, 56);
+            this.ToTextBox.Name = "ToTextBox";
+            this.ToTextBox.Size = new System.Drawing.Size(399, 38);
+            this.ToTextBox.TabIndex = 9;
+            // 
+            // FromTextBox
+            // 
+            this.FromTextBox.Location = new System.Drawing.Point(12, 12);
+            this.FromTextBox.Name = "FromTextBox";
+            this.FromTextBox.Size = new System.Drawing.Size(399, 38);
+            this.FromTextBox.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 532);
+            this.ControlBox = false;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MessageTextBox);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.ToTextBox);
+            this.Controls.Add(this.FromTextBox);
+            this.Name = "Form1";
+            this.Text = "msgfiles - Send Files";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox MessageTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.TextBox ToTextBox;
+        private System.Windows.Forms.TextBox FromTextBox;
+    }
+}
+
